842a8cd6a81bd5c1d47dfe01538651b3bbe28477
MAGNAT\sergey_ra <rakcheev.sg@magnat.ru>
2024-04-24 23:13:31 +0300

    style: Исправить code style

--- stats/stats-client/src/main/java/ru/practicum/ewm/client/stats/App.java
+++ stats/stats-client/src/main/java/ru/practicum/ewm/client/stats/App.java
@@ -1,10 +1,7 @@
 package ru.practicum.ewm.client.stats;
 
-
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
