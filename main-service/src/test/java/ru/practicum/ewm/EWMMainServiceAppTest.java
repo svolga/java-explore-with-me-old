cf91c012fef6860457df999721887e640cd7f3b9
MAGNAT\sergey_ra <rakcheev.sg@magnat.ru>
2024-04-24 23:21:39 +0300

    style: Исправить code style

--- main-service/src/test/java/ru/practicum/ewm/EWMMainServiceAppTest.java
+++ main-service/src/test/java/ru/practicum/ewm/EWMMainServiceAppTest.java
@@ -4,35 +4,28 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
 public class EWMMainServiceAppTest
-    extends TestCase
-{
+        extends TestCase {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public EWMMainServiceAppTest(String testName )
-    {
-        super( testName );
+    public EWMMainServiceAppTest(String testName) {
+        super(testName);
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( EWMMainServiceAppTest.class );
+    public static Test suite() {
+        return new TestSuite(EWMMainServiceAppTest.class);
     }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testApp() {
+        assertTrue(true);
     }
 }
